6666df6c0cdba436bc29422a1688281ea340f69f
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2022-09-03 11:50:46 -0700

    Create archive-script.js

---
+++ archive-script.js
@@ -0,0 +1,54 @@
+const usaMadeInInput = document.querySelector('.made__in--usa-input1');
+const madeInTheUSAInput = document.querySelector('.made__in--usa-label1');
+const madeInUSACount = document.querySelector('.wpf_item_count');
+let usaSwitchFunc = () => {document.querySelector('.iCheck-helper').click();
+							  };
+// 	if(document.querySelector('.woof_checkbox_term_6391').checked) {
+// 								  usaMadeInInput.checked = true;
+// 							  } else {
+// 								    usaMadeInInput.checked = false;
+// 							  }
+madeInTheUSAInput.addEventListener('click', usaSwitchFunc);
+	// Check on load
+// window.onload = function() {
+//   $(function(){
+//     var test = localStorage.usaMadeInInput === 'true'? true: false;
+//     $('input').prop('checked', test || false);
+// 	});
+
+// 	$('input').on('change', function() {
+// 		localStorage.usaMadeInInput = $(this).is(':checked');
+// 		console.log($(this).is(':checked'));
+// 	});
+// };
+
+// 	CHECKBOX
+let isUSACheck;
+if(window.location.href === 'https://typkup.com/shop/' ) {
+  isUSACheck = false;
+} else {
+  isUSACheck = JSON.parse(localStorage.getItem('isUSACheck')) === true ? true : window.location.href.indexOf('product_tag=usa') > 0 ? true : false;
+}
+localStorage.setItem('isUSACheck', isUSACheck)
+console.log(isUSACheck);
+document.querySelector('.made__in--usa-label1').addEventListener('click', () => console.log('clicked'));
+let isUSACheck_d = JSON.parse(localStorage.getItem('isUSACheck'));	
+	
+if (isUSACheck_d && window.location.href.indexOf('product_tag=usa') < 0 ) {
+	 window.location.assign(window.location.href+'?swoof=1&product_tag=usa');
+}
+if (window.location.href.indexOf('product_tag=usa') > 0) {
+    document.getElementById('switch').checked = true;
+    madeInTheUSAInput.addEventListener('click', () => {
+		isUSACheck = false; 
+		localStorage.setItem('isUSACheck', isUSACheck)});
+}
+	
+// if (document.getElementById('switch').checked && window.location.href.indexOf('product_tag=usa') < 0) {
+// 	window.location.assign(window.location.href+"/?swoof=1&product_tag=usa");
+// 	console.log(window.location.href);
+// 	console.log(document.getElementById('switch').checked)
+// 	} else {
+// 		console.log(window.location.href);
+// 		console.log(document.getElementById('switch').checked)
+// }
